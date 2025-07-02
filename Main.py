7a6f56337e8c318dd1ff86b74f1ec2ee77f2c66c
Ranjeet Kulkarni <ranjeetkulkarni2505@gmail.com>
2025-07-02 12:34:09 +0530

    Update Main.py with new logic/changes

--- Main.py
+++ Main.py
@@ -117,4 +117,6 @@ def test_supabase():
     return str(test_supabase_connection())
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
